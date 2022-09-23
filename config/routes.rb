92ba0b416572e55c1d37d92b7128f06686ce18fc
Dennis Kamau <97116157+devcamke@users.noreply.github.com>
2022-09-23 10:01:24 +0300

    Generate Account & configure Root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "accounts#index"
 end
